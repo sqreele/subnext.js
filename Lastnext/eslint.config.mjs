b6b7909c833eb18fe0a52419ece2acf0bcca7766
sqreele <kkanthong@gmail.com>
2025-03-15 17:06:54 +0700

    addfixdockerfilep92

--- Lastnext/eslint.config.mjs
+++ Lastnext/eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "jsx-a11y/alt-text": "warn",
+      "react/display-name": "warn",
+      "react/no-unescaped-entities": "warn",
+      "@next/next/no-img-element": "warn"
+    }
+  }
 ];
-
 export default eslintConfig;
