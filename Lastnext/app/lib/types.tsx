cd3b3251a1759842ab14e4da1b541e019c17c96c
sqreele <kkanthong@gmail.com>
2025-03-27 23:36:19 +0700

    addfiter5688lop5p

--- Lastnext/app/lib/types.tsx
+++ Lastnext/app/lib/types.tsx
@@ -85,7 +85,7 @@ export interface Room {
   created_at: string;
   property_id?: string | number;
   property?: number| string;
-  properties?:number;
+  properties?: number[];
 }
 
 export interface Job {
